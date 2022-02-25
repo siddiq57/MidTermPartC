13f54bdf040b2229e0f2b9eceec81e57ccf9b538
siddiq57 <98172700+siddiq57@users.noreply.github.com>
2022-02-25 10:41:44 -0500

    Initial commit

---
+++ src/midtermexam_versiona_extensioncode/TestUserprofile.java
@@ -0,0 +1,27 @@
+package midtermexam_versiona_extensioncode;
+import java.util.*;
+/**
+ * @author ahmad
+ */
+public class TestUserprofile {
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Please enter your name: ");
+        String name = input.nextLine();
+       
+        System.out.println("Please choose your favorite genre: ");
+        
+        System.out.println("Comedy, Drama, Action, Mystery");
+        String genre = input.nextLine();
+        
+        System.out.println("Your profile was succesfully created");
+        
+
+        
+       
+    }
+    
+}
